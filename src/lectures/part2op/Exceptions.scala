0e0bb1a7d7d1b606824892705f60106f4b9b624b
Gabriel Gonzalez <gabokappa@gmail.com>
2020-01-18 11:36:41 +0000

    Lesson 23 - start of Packaging and Imports

--- src/lectures/part2op/Exceptions.scala
+++ src/lectures/part2op/Exceptions.scala
@@ -1,5 +1,64 @@
 package lectures.part2op
 
-object Exceptions {
+import java.nio.BufferUnderflowException
+
+object Exceptions extends App {
+
+  // 1. Crash your program with OutofMemoryError
+
+//  val array = Array.ofDim(Int.MaxValue)
+
+  // 2 Crash with StackOverflowError with an infinite recurssive error
+
+//  def infinite: Int = 1 + infinite
+//  val noLimit = infinite
+
+  // 3 PocketCalculator that can add, subtract, multiply, divide and also throw some OverflowExeptions. UndeflowExceptions and MathcalculationException
+
+  class OverflowException extends RuntimeException
+  class UnderflowException extends RuntimeException
+  class MathCalculationException extends RuntimeException("Hey, you can't divide by 0")
+  object PocketCalculator {
+    def add(x: Int, y: Int)= {
+      val result = x + y
+
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def subtract(x: Int, y: Int) = {
+      val result = x -y
+      if (x > 0 && y < 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y > 0 && result > 0 ) throw new UnderflowException
+      else result
+    }
+
+    def multiply(x: Int, y: Int) = {
+      val result = x * y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result < 0) throw new OverflowException
+      else if (x > 0 && y < 0 && result > 0) throw new UnderflowException
+      else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def divide(x: Int, y: Int):Float = {
+      if (y == 0) throw new MathCalculationException
+      else x.toFloat / y.toFloat
+
+    }
+
+  }
+
+    /*
+  try {
+    // compute a value
+  } catch {
+    case e: RuntimeException => // another value
+  } finally {
+    // block for side effects or code that WILL ALWAYS RUN NO MATTER WHAT
+  }
+     */
 
 }
