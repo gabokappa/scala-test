65c16c4e274e2ee7ed111119a2c127c4fe0fe225
Gabriel Gonzalez <gabokappa@gmail.com>
2020-01-19 19:12:53 +0000

    Started section 26

--- src/lectures/part3fp/HOFsCurries.scala
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -6,15 +6,16 @@ val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = nu
 
   // Higher order function
 
-  // functio nthat applies another functio nn times over a value x
+  // function that applies another function n times over a value x
   // nTimes(f, n, x) f = function, n = times, x = subject
+  // nTimes(f, 3, x) is equivalent to f(f(f(x)) = nTimes(f, 2, f(x))
 
   def nTimes(f: Int => Int, n: Int, x: Int): Int =
     if (n <= 0) x
     else nTimes(f, n-1, f(x))
 
   val plusOne = (x: Int) => x + 1
-  println(nTimes(plusOne, 10, 1))
+  println(s"printing plusone: ${nTimes(plusOne, 10, 1)}")
 
   /* the immediate above then equates to
   nTimes(plusOne, 9, 2)
